09900fd06080ab2930d98bc1b43c54d449030613
niux3 <renaudbourdeau@gmail.com>
2026-01-03 18:35:02 +0100

    fix: text url api

--- frontend/src/lib/Header.svelte
+++ frontend/src/lib/Header.svelte
@@ -29,13 +29,13 @@
         <li>
             <p class="instruction">Format <strong>JSON</strong> (par défaut) :</p>
             <CopyBlock content='curl -s "https://rb-webstudio.go.yj.fr/chuck-norris-ipsum/api/?sentence_min=1&sentence_max=5&paragraph=2"' label="Copier">
-                <pre><code>curl -s "http://localhost:5000/api/?sentence_min=1&amp;sentence_max=5&amp;paragraph=2"</code></pre>
+                <pre><code>curl -s "https://rb-webstudio.go.yj.fr/chuck-norris-ipsum/api?sentence_min=1&amp;sentence_max=5&amp;paragraph=2"</code></pre>
             </CopyBlock>
         </li>
         <li>
             <p class="instruction">Format <strong>Texte brut</strong> (idéal pour <a href="https://linux.die.net/man/1/xclip" target="_blank">xclip</a>/scripts) :</p>
             <CopyBlock content='curl -s "https://rb-webstudio.go.yj.fr/chuck-norris-ipsum/api/?sentence_min=1&sentence_max=5&paragraph=2&text=1"' label="Copier">
-                <pre><code>curl -s "http://localhost:5000/api/?sentence_min=1&amp;sentence_max=5&amp;paragraph=2&amp;text=1"</code></pre>
+                <pre><code>curl -s "https://rb-webstudio.go.yj.fr/chuck-norris-ipsum/api/?sentence_min=1&amp;sentence_max=5&amp;paragraph=2&amp;text=1"</code></pre>
             </CopyBlock>
         </li>
     </ol>
