55821d909ec8b64a52f92e1750120de37191abc6
niux3 <renaudbourdeau@gmail.com>
2026-01-09 19:00:54 +0100

    feat: add slidshow

--- frontend/src/lib/Illustration.svelte
+++ frontend/src/lib/Illustration.svelte
@@ -1,1 +1,9 @@
-<div class="container"><div class="illustration"><img src="./chuck_norris.jpg" alt="Chuck Norris" loading="lazy"></div></div>
+<div class="container wrap-illustration">
+    <div class="illustration">
+        <img src="./chuck_norris.jpg" alt="Chuck Norris" loading="lazy">
+        <img src="./eyes_for_eyes.jpg" alt="Chuck Norris" loading="lazy">
+        <img src="./braddock.jpg" alt="Chuck Norris" loading="lazy">
+        <img src="./code_of_silence.jpg" alt="Chuck Norris" loading="lazy">
+        <img src="./delta_force.jpg" alt="Chuck Norris" loading="lazy">
+    </div>
+</div>
